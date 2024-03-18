41cf0e44c95401d6e649e4b112f588f0ab1f149c
VoidLeech <ian.gerritse@gmail.com>
2024-03-18 23:42:58 +0100

    Eliminate weekday type, use DayOfWeek from Data.Time.Calendar instead
    Some documentation, remove unnecessary Just and >>= in parseDelayRequest

--- src/MessageFormat.hs
+++ src/MessageFormat.hs
@@ -10,6 +10,7 @@ import Data.Text (unpack)
 import Data.Text.Encoding (decodeUtf8)
 import Data.List
 import Text.Read
+import Data.Time.Calendar
 
 data StopPair = StopPair {stopName :: String, stopId :: Int} deriving Generic
 data Stops = Stops {stops :: [StopPair]} deriving Generic
@@ -30,25 +31,26 @@ toBusPair (s, i) = BusPair s i
 data Delays = Delays {departureDelay :: Int, destinationDelay :: Int} deriving Generic
 instance ToJSON Delays where
 
-data DelayRequest = DelayRequest {departure :: Int, destination :: Int, bus :: Int, weekday :: Weekday, hour :: Int, minute :: Int}
+data DelayRequest = DelayRequest {departure :: Int, destination :: Int, bus :: Int, weekday :: DayOfWeek, hour :: Int, minute :: Int}
 
--- Temp location for this type
-data Weekday = Mon | Tue | Wed | Thu | Fri | Sat | Sun
-
-validWeekday :: String -> Maybe Weekday
-validWeekday "0" = Just Mon
-validWeekday "1" = Just Tue
-validWeekday "2" = Just Wed
-validWeekday "3" = Just Thu
-validWeekday "4" = Just Fri
-validWeekday "5" = Just Sat
-validWeekday "6" = Just Sun
+-- | Tries to parse a string to a weekday according to the encoded representation.
+-- Week starts at Monday, index 0, and so on.
+validWeekday :: String -> Maybe DayOfWeek
+validWeekday "0" = Just Monday
+validWeekday "1" = Just Tuesday
+validWeekday "2" = Just Wednesday
+validWeekday "3" = Just Thursday
+validWeekday "4" = Just Friday
+validWeekday "5" = Just Saturday
+validWeekday "6" = Just Sunday
 validWeekday  _  = Nothing
 
+-- | Turns numbers outside the valid hour range (0-23) into Nothing.
 validHour :: Int -> Maybe Int
 validHour h | h >= 0 && h < 24 = Just h
             | otherwise        = Nothing
 
+-- | Turns 'Int's outside the valid minute range (0-59) into Nothing.
 validMinute :: Int -> Maybe Int
 validMinute h | h >= 0 && h < 60 = Just h
             | otherwise        = Nothing
@@ -58,12 +60,12 @@ type ServerData = ()
 
 parseDelayRequest :: Query -> Maybe DelayRequest
 parseDelayRequest q = do
-  qDeparture   <- find ((== "departure")   . fst) q >>= snd >>= Just . unpack . decodeUtf8 >>= readMaybe @Int
-  qDestination <- find ((== "destination") . fst) q >>= snd >>= Just . unpack . decodeUtf8 >>= readMaybe @Int
-  qBus         <- find ((== "bus")         . fst) q >>= snd >>= Just . unpack . decodeUtf8 >>= readMaybe @Int
-  qWeekday     <- find ((== "weekday")     . fst) q >>= snd >>= validWeekday . unpack . decodeUtf8
-  qHour        <- find ((== "hour")        . fst) q >>= snd >>= Just . unpack . decodeUtf8 >>= readMaybe @Int >>= validHour
-  qMinute      <- find ((== "minute")      . fst) q >>= snd >>= Just . unpack . decodeUtf8 >>= readMaybe @Int >>= validMinute
+  qDeparture   <- find ((== "departure")   . fst) q >>= snd >>= readMaybe @Int . unpack . decodeUtf8
+  qDestination <- find ((== "destination") . fst) q >>= snd >>= readMaybe @Int . unpack . decodeUtf8
+  qBus         <- find ((== "bus")         . fst) q >>= snd >>= readMaybe @Int . unpack . decodeUtf8
+  qWeekday     <- find ((== "weekday")     . fst) q >>= snd >>= validWeekday   . unpack . decodeUtf8
+  qHour        <- find ((== "hour")        . fst) q >>= snd >>= readMaybe @Int . unpack . decodeUtf8 >>= validHour
+  qMinute      <- find ((== "minute")      . fst) q >>= snd >>= readMaybe @Int . unpack . decodeUtf8 >>= validMinute
   return $ DelayRequest qDeparture qDestination qBus qWeekday qHour qMinute
 
 validateDelayRequest :: DelayRequest -> ServerData -> Bool
