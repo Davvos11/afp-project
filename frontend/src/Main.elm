b01f99d4e26299e62ddaacb89fd1b5138e0b0440
VoidLeech <ian.gerritse@gmail.com>
2024-03-19 01:17:58 +0100

    Frontend documentation/small refactors

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -23,60 +23,64 @@ main = Browser.element {init = init,
                         subscriptions = subscriptions,
                         view = view}
 
+-- | Initializes model and creates request (that will chain into other requests) for dynamic initial data
 init : () -> (Model, Cmd Msg)
-init _ = (Model {departure   = NoStop "",
-                 destination = NoStop "",
-                 bus         = NoBus "",
-                 moment      = Moment {day = Time.Mon, hour = 0, minute = 0},
-                 delays      = Nothing,
-                 stops       = Dict.empty,
-                 buses       = Dict.empty,
-                 loading     = NotLoading,
-                 time        = (Time.millisToPosix 0, utc)
+init _ = (Model {departure    = NoStop "",
+                 destination  = NoStop "",
+                 bus          = NoBus "",
+                 momentInWeek = MomentInWeek {day = Time.Mon, hour = 0, minute = 0},
+                 delays       = Nothing,
+                 stops        = Dict.empty,
+                 buses        = Dict.empty,
+                 loading      = NotLoading,
+                 time         = (Time.millisToPosix 0, utc)
                 },
           Task.perform identity (Task.map2 NewTime Time.now Time.here))
 
-
+-- | Takes a list of paired names/ids, and turns them into dicts for looking up the proper name/ids based on the lowercase name
 toDict : List (String, Int) -> Dict.Dict String (Int, String)
 toDict xs = Dict.fromList (List.map (\(x, i) -> (String.toLower x, (i, x))) xs)
 
+-- | Updates the model
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg (Model m) = case msg of
-    ReverseStops    -> (Model {m | departure = m.destination, destination = m.departure}, Cmd.none)
-    NewTime t z     -> (Model {m | time = (t, z), moment = Moment {day = toWeekday z t, hour = toHour z t, minute = toMinute z t}}, requestStops)
-    TimeChange t    -> (Model {m | moment = (case t of
-                                               Minus5Min  -> subMinute m.moment 5
-                                               Minus15Min -> subMinute m.moment 15
-                                               MinusHour  -> subMinute m.moment 60
-                                               Plus5Min   -> addMinute m.moment 5
-                                               Plus15Min  -> addMinute m.moment 15
-                                               PlusHour   -> addMinute m.moment 60
-                        )}, Cmd.none)
-    DayChange d     -> (Model {m | moment = (\(Moment moment) -> Moment {moment | day = d}) m.moment}, Cmd.none)
-    StopChange x d  -> (Model (case d of
-                           -- Halte changed, needs to be revalidated
-                           -- Todo reset delays when these change
-                           Departure   -> {m | departure   = NoStop x}
-                           Destination -> {m | destination = NoStop x}
-                      ), Cmd.none)
-                           -- Same for the bus
-    BusChange x     -> (Model {m | bus = NoBus x}, Cmd.none)
-    ValidateBus     -> (Model {m | bus = tryParseBus (m.bus) (m.buses)}, Cmd.none)
-    ValidateStop d  -> (Model (case d of
-                           Departure   -> {m | departure   = tryParseStop m.departure   m.stops}
-                           Destination -> {m | destination = tryParseStop m.destination m.stops}
-                        ), Cmd.none)
-    CalcDelay       -> (Model {m | loading = Loading}, requestDelays (Model m))
+    -- Initialization
+    NewTime t z     -> (Model {m | time = (t, z), momentInWeek = MomentInWeek {day = toWeekday z t, hour = toHour z t, minute = toMinute z t}}, requestStops)
     GotStops res    -> case res of
                          Ok stops -> (Model {m | stops = toDict stops}, requestBuses)
                          Err _    -> (Model m, Cmd.none)
     GotBuses res    -> case res of
                          Ok buses -> (Model {m | buses = toDict buses}, Cmd.none)
                          Err _    -> (Model m, Cmd.none)
+    TimeChange t    -> (Model {m | momentInWeek = (case t of
+                                                     Minus5Min  -> subMinute m.momentInWeek 5
+                                                     Minus15Min -> subMinute m.momentInWeek 15
+                                                     MinusHour  -> subMinute m.momentInWeek 60
+                                                     Plus5Min   -> addMinute m.momentInWeek 5
+                                                     Plus15Min  -> addMinute m.momentInWeek 15
+                                                     PlusHour   -> addMinute m.momentInWeek 60
+                        )}, Cmd.none)
+    DayChange d     -> (Model {m | momentInWeek = (\(MomentInWeek momentInWeek) -> MomentInWeek {momentInWeek | day = d}) m.momentInWeek}, Cmd.none)
+    StopChange x d  -> (Model (case d of
+                           -- Stop changed, needs to be revalidated
+                           -- Todo reset delays when these change
+                           Departure   -> {m | departure   = NoStop x}
+                           Destination -> {m | destination = NoStop x}
+                      ), Cmd.none)
+                           -- Same for the bus
+    BusChange x     -> (Model {m | bus = NoBus x}, Cmd.none)
+    ReverseStops    -> (Model {m | departure = m.destination, destination = m.departure}, Cmd.none)
+    ValidateBus     -> (Model {m | bus = tryParseBus (m.bus) (m.buses)}, Cmd.none)
+    ValidateStop d  -> (Model (case d of
+                           Departure   -> {m | departure   = tryParseStop m.departure   m.stops}
+                           Destination -> {m | destination = tryParseStop m.destination m.stops}
+                        ), Cmd.none)
+    CalcDelay       -> (Model {m | loading = Loading}, requestDelays (Model m))
     GotDelays res   -> case res of
                          Ok (d1, d2) -> (Model {m | delays = Just (d1, d2), loading = NotLoading}, Cmd.none)
                          Err _       -> (Model {m | loading = NotLoading}, Cmd.none)
 
+-- | Attempts to find the name in a bus field in its known dict of buses to create an identified bus
 tryParseBus : Bus -> Dict.Dict String (Int, String) -> Bus
 tryParseBus b dict = case b of
     Bus i f -> Bus i f
@@ -84,6 +88,7 @@ tryParseBus b dict = case b of
                Nothing          -> NoBus x
                Just (i, xprime) -> Bus i (always xprime)
 
+-- | Attempts to find the name in a stop field in its known dict of stops to create an identified stop
 tryParseStop : Stop -> Dict.Dict String (Int, String) -> Stop
 tryParseStop s dict = case s of
     Stop i f -> Stop i f
@@ -91,8 +96,9 @@ tryParseStop s dict = case s of
                Nothing          -> NoStop x
                Just (i, xprime) -> Stop i (always xprime)
 
+-- | Creates the request to calculate the delays for this bus and these stops
+-- Idea to consider and discuss: don't send request if departure/destination are equal, or keep as feature instead to poll only a single stop
 requestDelays : Model -> Cmd Msg
--- Todo: Request delays using model data
 requestDelays (Model m) = case (m.departure, m.destination, m.bus) of
   (Stop i1 _, Stop i2 _, Bus i3 _) -> Http.request
     { method = "GET",
@@ -100,9 +106,9 @@ requestDelays (Model m) = case (m.departure, m.destination, m.bus) of
       url = Url.Builder.crossOrigin "http://localhost:3000" ["delays"] [Url.Builder.int "departure" i1,
                                                                         Url.Builder.int "destination" i2,
                                                                         Url.Builder.int "bus" i3,
-                                                                        Url.Builder.int "day" ((\(Moment moment) -> toDayNumber moment.day) m.moment),
-                                                                        Url.Builder.int "hour" ((\(Moment moment) -> moment.hour) m.moment),
-                                                                        Url.Builder.int "minute" ((\(Moment moment) -> moment.minute) m.moment)
+                                                                        Url.Builder.int "day"    ((\(MomentInWeek momentInWeek) -> toDayNumber momentInWeek.day)    m.momentInWeek),
+                                                                        Url.Builder.int "hour"   ((\(MomentInWeek momentInWeek) ->             momentInWeek.hour)   m.momentInWeek),
+                                                                        Url.Builder.int "minute" ((\(MomentInWeek momentInWeek) ->             momentInWeek.minute) m.momentInWeek)
                                                                        ],
       body = Http.emptyBody,
       expect = Http.expectJson GotDelays
@@ -112,35 +118,34 @@ requestDelays (Model m) = case (m.departure, m.destination, m.bus) of
     }
   (_, _, _) -> Cmd.none
 
--- The below two could be factoried away slightly
+-- | Factory method for the static buses/stops requests
+requestStatic : String -> String -> ((Result Http.Error (List (String, Int))) -> Msg) -> Cmd Msg
+requestStatic singular plural msgToSend = Http.request { method = "GET",
+                                                         headers = [],
+                                                         url = Url.Builder.crossOrigin "http://localhost:3000" [plural] [],
+                                                         body = Http.emptyBody,
+                                                         expect = Http.expectJson msgToSend (field plural (list (Json.Decode.map2 (\a b -> (a, b)) (field (singular ++ "Name") string) (field (singular ++ "Id") int)))),
+                                                         timeout = Nothing,
+                                                         tracker = Nothing
+                                                       }
 requestStops : Cmd Msg
-requestStops = Http.request { method = "GET",
-                              headers = [],
-                              url = "http://localhost:3000/stops",
-                              body = Http.emptyBody,
-                              expect = Http.expectJson GotStops (field "stops" (list (Json.Decode.map2 (\a b -> (a, b)) (field "stopName" string) (field "stopId" int)))),
-                              timeout = Nothing,
-                              tracker = Nothing
-                            }
+requestStops = requestStatic "stop" "stops" GotStops
 
 requestBuses : Cmd Msg
-requestBuses = Http.request { method = "GET",
-                              headers = [],
-                              url = "http://localhost:3000/buses",
-                              body = Http.emptyBody,
-                              expect = Http.expectJson GotBuses (field "buses" (list (Json.Decode.map2 (\a b -> (a, b)) (field "busName" string) (field "busId" int)))),
-                              timeout = Nothing,
-                              tracker = Nothing
-                            }
+requestBuses = requestStatic "bus" "buses" GotBuses
 
+-- | How to display the model
+-- Massive todo, turn this into smaller, digestable bits
 view : Model -> Html Msg
-view (Model m) = div [] [button [onClick (TimeChange Minus5Min)] [text "-5"],
+view (Model m) = div [] [ -- Time changes, display in between - & +
+                         button [onClick (TimeChange Minus5Min)] [text "-5"],
                          button [onClick (TimeChange Minus15Min)] [text "-15"],
                          button [onClick (TimeChange MinusHour)] [text "-60"],
-                         text ((\(Moment moment) -> (if moment.hour < 10 then "0" else "") ++ String.fromInt moment.hour ++ ":" ++ (if moment.minute < 10 then "0" else "") ++ String.fromInt moment.minute) m.moment),
+                         text ((\(MomentInWeek momentInWeek) -> (if momentInWeek.hour < 10 then "0" else "") ++ String.fromInt momentInWeek.hour ++ ":" ++ (if momentInWeek.minute < 10 then "0" else "") ++ String.fromInt momentInWeek.minute) m.momentInWeek),
                          button [onClick (TimeChange PlusHour)] [text "+60"],
                          button [onClick (TimeChange Plus15Min)] [text "+15"],
                          button [onClick (TimeChange Plus5Min)] [text "+5"],
+                         -- Day selection
                          div [] [button [onClick (DayChange Time.Mon)] [text "Ma"],
                                  button [onClick (DayChange Time.Tue)] [text "Di"],
                                  button [onClick (DayChange Time.Wed)] [text "Wo"],
@@ -148,12 +153,14 @@ view (Model m) = div [] [button [onClick (TimeChange Minus5Min)] [text "-5"],
                                  button [onClick (DayChange Time.Fri)] [text "Vr"],
                                  button [onClick (DayChange Time.Sat)] [text "Za"],
                                  button [onClick (DayChange Time.Sun)] [text "Zo"],
-                                 text (toDutchWeekday ((\(Moment moment) -> moment.day) m.moment))],
+                                 text (toDutchWeekday ((\(MomentInWeek momentInWeek) -> momentInWeek.day) m.momentInWeek))],
+                        -- Stop input
                          div [] ([input [placeholder "Vertrekhalte",
                                          value (case m.departure of
                                                   Stop i f -> f ()
                                                   NoStop x -> x),
                                          onInput (\x -> StopChange x Departure)] [],
+                                  -- Validation. Shows whether the input parsed. Shows delay if calculated instead
                                   button [onClick (ValidateStop Departure)] [text "Check"]] ++ (case m.departure of
                                                                                                   NoStop x -> []
                                                                                                   Stop i f -> case m.delays of
@@ -165,11 +172,13 @@ view (Model m) = div [] [button [onClick (TimeChange Minus5Min)] [text "-5"],
                                                   Stop i f -> f ()
                                                   NoStop x -> x),
                                          onInput (\x -> StopChange x Destination)] [],
+                                  -- Validation. Shows whether the input parsed. Shows delay if calculated instead
                                   button [onClick (ValidateStop Destination)] [text "Check"]] ++ (case m.destination of
                                                                                                   NoStop x -> []
                                                                                                   Stop i f -> case m.delays of
                                                                                                                 Nothing       -> ([text "Geldige halte"])
                                                                                                                 Just (d1, d2) -> [text (if d2 == 0 then "Op tijd!" else ("+" ++ (String.fromInt d2)))])),
+                         -- Bus input
                          div [] ([input [placeholder "Bus",
                                          value (case m.bus of
                                                   Bus i f -> f ()
@@ -181,12 +190,15 @@ view (Model m) = div [] [button [onClick (TimeChange Minus5Min)] [text "-5"],
                          div [] [button [onClick CalcDelay] [text "Voorspel vertraging"]]
                 ]
 
-subMinute : Moment -> Int -> Moment
-subMinute (Moment m) x = Moment {m | hour=if m.minute - x < 0 then (if m.hour - 1 < 0 then m.hour + 23 else m.hour - 1) else m.hour, minute=if m.minute - x < 0 then m.minute - x + 60 else m.minute - x}
+-- | Subtracts up to 60 minutes from a given MomentInWeek. Does not change 'day' on wrap-around
+subMinute : MomentInWeek -> Int -> MomentInWeek
+subMinute (MomentInWeek m) x = MomentInWeek {m | hour=if m.minute - x < 0 then (if m.hour - 1 < 0 then m.hour + 23 else m.hour - 1) else m.hour, minute=if m.minute - x < 0 then m.minute - x + 60 else m.minute - x}
 
-addMinute : Moment -> Int -> Moment
-addMinute (Moment m) x = Moment {m | hour=if m.minute + x > 59 then (if m.hour + 1 > 23 then m.hour - 23 else m.hour + 1) else m.hour, minute=if m.minute + x > 59 then m.minute + x - 60 else m.minute + x}
+-- | Adds up to 60 minutes to a given MomentInWeek. Does not change 'day' on wrap-around.
+addMinute : MomentInWeek -> Int -> MomentInWeek
+addMinute (MomentInWeek m) x = MomentInWeek {m | hour=if m.minute + x > 59 then (if m.hour + 1 > 23 then m.hour - 23 else m.hour + 1) else m.hour, minute=if m.minute + x > 59 then m.minute + x - 60 else m.minute + x}
 
+-- | Creates a string representation of a weekday, in Dutch
 toDutchWeekday : Time.Weekday -> String
 toDutchWeekday t = (case t of
                       Time.Mon -> "Maan"
@@ -198,6 +210,8 @@ toDutchWeekday t = (case t of
                       Time.Sun -> "Zon")
                     ++ "dag"
 
+-- | Events to poll all the occasionally / frequently, without user input
+-- Idea to consider: do this with stops/bus parsing or requesting the stop / bus data again if these are still empty
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
--- frontend/src/Model.elm
+++ frontend/src/Model.elm
@@ -6,35 +6,26 @@ import Dict
 type Model = Model {departure : Stop,
                     destination : Stop,
                     bus : Bus,
-                    moment : Moment,
-                    delays : DelayInfo,
-                    stops : Dict.Dict String (Int, String),
-                    buses : Dict.Dict String (Int, String),
+                    momentInWeek : MomentInWeek,
+                    delays : DelayInfo,                     --^ Calculate delay data, if any
+                    stops : Dict.Dict String (Int, String), --^ Key: lowercased name, Value: (id, name proper)
+                    buses : Dict.Dict String (Int, String), --^ Key: lowercased name, Value: (id, name proper)
                     loading : LoadingState,
-                    time : (Time.Posix, Time.Zone)}
+                    time : (Time.Posix, Time.Zone)          --^ Current time, only used for initialization
+                   }
 
-type Moment = Moment {day : Time.Weekday, hour : Int, minute : Int}
+type MomentInWeek = MomentInWeek {day : Time.Weekday, hour : Int, minute : Int}
 
-toDayNumber : Time.Weekday -> Int
-toDayNumber t = case t of
-                  Time.Mon -> 0
-                  Time.Tue -> 1
-                  Time.Wed -> 2
-                  Time.Thu -> 3
-                  Time.Fri -> 4
-                  Time.Sat -> 5
-                  Time.Sun -> 6
+type Stop = Stop Int (() -> String) --^ An identified stop, storing its id and a function that will always return the same string (to display the correct name)
+          | NoStop String           --^ Current value in the input field, not (yet) succesfully parsed as a stop
 
-type Stop = Stop Int (() -> String)
-          | NoStop String
+type Bus = Bus Int (() -> String) --^ An identified bus, storing its id and a function that will always return the same string (to display the correct name)
+         | NoBus String           --^ Current value in the input field, not (yet) succesfully parsed as a bus
 
-type Direction = Departure
-               | Destination
-
-type Bus = Bus Int (() -> String)
-         | NoBus String
-
+-- | Whether we're waiting on a response from the server.
+-- Currently tracked but not used
 type LoadingState = Loading
                   | NotLoading
 
+-- | Average delay in minutes at (departure, destination)
 type alias DelayInfo = Maybe (Int, Int)
