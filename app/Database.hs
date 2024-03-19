d13546da8091ae9d0e804039b616d77abef7c90a
David Vos <vosdavid2@gmail.com>
2024-03-19 15:58:26 +0100

    add day of week to filters

--- app/Database.hs
+++ app/Database.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Database where
 
@@ -8,21 +9,44 @@ import qualified Data.Text as Text
 import Text.Printf
 
 data TimeOfDay = TimeOfDay { hours :: Int, minutes :: Int }
-    deriving (Show)
 
-timeToString :: TimeOfDay -> String
-timeToString (TimeOfDay h m) = printf "%02d" h ++ ":" ++ printf "%02d" m ++ ":00"
+instance Show TimeOfDay where
+    show :: TimeOfDay -> String
+    show (TimeOfDay h m) = printf "%02d" h ++ ":" ++ printf "%02d" m ++ ":00"
+
+data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+instance Show DayOfWeek where
+  show :: DayOfWeek -> String
+  show Mon = "Monday"
+  show Tue = "Tuesday"
+  show Wed = "Wednesday"
+  show Thu = "Thursday"
+  show Fri = "Friday"
+  show Sat = "Saturday"
+  show Sun = "Sunday"
+
+weekToNumber :: DayOfWeek -> String
+weekToNumber Mon = "1"
+weekToNumber Tue = "2"
+weekToNumber Wed = "3"
+weekToNumber Thu = "4"
+weekToNumber Fri = "5"
+weekToNumber Sat = "6"
+weekToNumber Sun = "7"
 
 data Filter = Filter {
     stopId :: Maybe Int,
-    timeOfDay :: Maybe TimeOfDay
+    timeOfDay :: Maybe TimeOfDay,
+    dayOfWeek :: Maybe DayOfWeek
 } deriving Show
 
 -- | Get the frequency distribution of the punctualities for a given bus stop
 getFrequencies :: Filter -> IO [Frequency]
-getFrequencies (Filter mStopId mTimeOfDay) = do
+getFrequencies (Filter mStopId mTime mDay) = do
     conn <- open "database.db"
     print queryString
+    print parameters
     result <- queryNamed conn (Query queryString) parameters :: IO [Frequency]
     close conn
     return result
@@ -32,8 +56,9 @@ getFrequencies (Filter mStopId mTimeOfDay) = do
         
         (conditions, parameters) = foldr addFilter ([], []) [
                 fmap (\s -> ("stop_id = :stop_id", Text.pack ":stop_id" := s)) mStopId,
-                fmap (\t -> ("time(timestamp) >= time(:time_of_day_0, '-30 minutes')", ":time_of_day_0" := timeToString t)) mTimeOfDay,
-                fmap (\t -> ("time(timestamp) <= time(:time_of_day_1, '+30 minutes')", ":time_of_day_1" := timeToString t)) mTimeOfDay
+                fmap (\t -> ("time(timestamp) >= time(:time_of_day_0, '-30 minutes')", ":time_of_day_0" := show t)) mTime,
+                fmap (\t -> ("time(timestamp) <= time(:time_of_day_1, '+30 minutes')", ":time_of_day_1" := show t)) mTime,
+                fmap (\d -> ("strftime('%w', timestamp) = :day_of_week", ":day_of_week" := weekToNumber d)) mDay
             ]
 
         addFilter Nothing acc = acc
--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,17 @@
 module Main where
 
-import Subscriber
-import Parser
 import Database
+import Parser
 import Server
+import Subscriber
 
 main :: IO ()
 main = do
-  x <- Database.getFrequencies $ Filter {stopId = Nothing, timeOfDay = Just TimeOfDay {hours = 13, minutes = 0}}
+  x <- Database.getFrequencies $ Filter {
+    stopId = Nothing,
+    timeOfDay = Nothing,
+    dayOfWeek = Just Thu
+  }
   mapM_ print x
   Subscriber.run
   runServer
