79242e7347a14cb65016a670508950427ae9268c
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2024-03-21 22:00:51 +0100

    Add XML parse library

--- src/Subscriber.hs
+++ src/Subscriber.hs
@@ -5,6 +5,8 @@ module Subscriber (run) where
 -- Contains code from
 -- https://github.com/jaspervdj/websockets/blob/master/example/client.hs
 
+import           Data.Map (Map)
+import qualified Data.Map as Map
 import           Control.Monad       (forever)
 import           Control.Monad.Trans (liftIO)
 import           Network.Socket      (withSocketsDo)
@@ -16,6 +18,7 @@ import           Data.ByteString.Lazy as BS
 import           Data.Text.Lazy.Encoding as TLE
 import           Data.Text.Lazy as TL
 import qualified Codec.Compression.GZip as GZip
+import           Text.XML.Light.Input (parseXML, parseXMLDoc)
 
 run :: IO ()
 run = withSocketsDo $ WS.runClient "localhost" 9160 "/" app
@@ -27,7 +30,12 @@ limitText s =
         if (T.length a) > 30
         then a <> "..."
         else a
-       ) 
+       )
+
+kv6posinfoToUpdates :: BS.ByteString -> [Map String String]
+kv6posinfoToUpdates rawS =
+    let content = parseXML rawS
+    in error $ show content
 
 app :: WS.ClientApp ()
 app conn = do
@@ -51,6 +59,11 @@ app conn = do
 
                 WS.Text a _ -> ("text data (direct)", a)
                 )
+
+        let test = (if (msgType == "compressed text data") then
+                (kv6posinfoToUpdates msgData) else [])
+        liftIO $ putStrLn $ show test
+
         liftIO $ T.putStrLn $ ("Received " <> msgType <> ": " <> limitText msgData)
 
         pure ()
