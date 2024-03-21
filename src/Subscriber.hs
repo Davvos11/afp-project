1a8d41e8395d07a75c9e10548acff21dabfff1eb
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2024-03-21 18:18:16 +0100

    Refactor Subscriber

--- src/Subscriber.hs
+++ src/Subscriber.hs
@@ -8,6 +8,7 @@ module Subscriber (run) where
 import           Control.Monad       (forever)
 import           Control.Monad.Trans (liftIO)
 import           Network.Socket      (withSocketsDo)
+import           Data.Text           (Text)
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as T
 import qualified Network.WebSockets  as WS
@@ -19,6 +20,15 @@ import qualified Codec.Compression.GZip as GZip
 run :: IO ()
 run = withSocketsDo $ WS.runClient "localhost" 9160 "/" app
 
+limitText :: BS.ByteString -> Data.Text.Text
+limitText s =
+    let a = T.take 31 . TL.toStrict . TLE.decodeUtf8 $ s
+    in (
+        if (T.length a) > 30
+        then a <> "..."
+        else a
+       ) 
+
 app :: WS.ClientApp ()
 app conn = do
     putStrLn "Connected!"
@@ -29,18 +39,19 @@ app conn = do
         -- For converting ByteString to Text or String, see
         -- https://stackoverflow.com/questions/3232074/what-is-the-best-way-to-convert-string-to-bytestring
 
-        case msg of
-            WS.Binary a -> do
-                case BS.head a of
-                    0x1f -> do
-                        let decompressed = GZip.decompress a
-                        let decompressedText = (TL.toStrict $ TLE.decodeUtf8 decompressed)
-                        liftIO $ T.putStrLn ("Received binary-encoded compressed text data: " <> T.take 30 decompressedText <> "...")
+        let (msgType, msgData) = (
+                case msg of
+                WS.Binary a ->
+                    case BS.head a of
+                        0x1f ->
+                            let decompressed = GZip.decompress a
+                            in ("compressed text data", decompressed)
 
-                    _ -> liftIO $ T.putStrLn ("Received binary-encoded text data: " <> (TL.toStrict $ TLE.decodeUtf8 a))
-            WS.Text a _ -> do
-                liftIO $ T.putStrLn $ ("Receive text data: " <> (TL.toStrict $ TLE.decodeUtf8 a))
-            
+                        _ -> ("text data", a)
+
+                WS.Text a _ -> ("text data (direct)", a)
+                )
+        liftIO $ T.putStrLn $ ("Received " <> msgType <> ": " <> limitText msgData)
 
         pure ()
     -- WS.sendClose conn ("Exit" :: Text)
