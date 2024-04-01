8de1ddcb8adb4af9f9e97c10ab0e692895dd63c6
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2024-04-01 13:40:47 +0200

    Clean up Subscriber

--- src/Subscriber.hs
+++ src/Subscriber.hs
@@ -1,49 +1,86 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Subscriber (run) where
+module Subscriber (
+    runWrite,
+    runPrint,
+    PosInfo (..),
+    StopInfo (..),
+    printPosInfos,
+    writePosInfos,
+
+    listen,
+    listenKV6,
+
+    getStop,
+    includeUpdate,
+    filterConduit,
+
+    runPrintOld,
+) where
 
 -- Contains code from
 -- https://github.com/jaspervdj/websockets/blob/master/example/client.hs
 
-import           Data.Map            (Map, (!))
-import           Data.Maybe          (isJust)
-import qualified Data.Map as Map
-import qualified Data.Stream         as Stream
-import           Data.Stream         (Stream (..))
-import           Control.Monad       (forever, filterM, liftM)
-import           Control.Monad.Trans (liftIO)
-import           Network.Socket      (withSocketsDo)
-import           Data.Text           (Text)
-import qualified Data.Text           as T
-import qualified Data.Text.IO        as T
-import qualified Network.WebSockets  as WS
-import           Data.ByteString.Lazy as BS
+import           Data.Map                (Map, (!))
+import           Data.Maybe              (isJust)
+import qualified Data.Map                as Map
+import           Data.Text               (Text)
+import qualified Data.Text               as T
+import           Data.ByteString.Lazy    as BS
 import           Data.Text.Lazy.Encoding as TLE
-import           Data.Text.Lazy as TL
-import qualified Codec.Compression.GZip as GZip
-import           Text.XML.Light.Input (parseXMLDoc)
-import qualified Text.XML.Light.Types as XML
-import qualified Text.XML.Light.Proc as XMLProc
-import qualified Data.Conduit             as C
-import           Data.Conduit
-import qualified Database.SQLite.Simple   as SQL
-import Database.SQLite.Simple.ToField (toField)
+import           Data.Text.Lazy          as TL
+import           Control.Monad           (forever, filterM)
+import           Control.Monad.Trans     (liftIO)
 
-run :: IO ()
-run = do
-    listen writeToDatabase
+-- XML
+import           Text.XML.Light.Input    (parseXMLDoc)
+import qualified Text.XML.Light.Types    as XML
+import qualified Text.XML.Light.Proc     as XMLProc
+
+-- Conduits
+import qualified Data.Conduit            as C
+import           Data.Conduit
+
+-- WebSockets
+import qualified Network.WebSockets      as WS
+import qualified Codec.Compression.GZip  as GZip
+
+-- SQLite
+import qualified Database.SQLite.Simple  as SQL
+import Database.SQLite.Simple.ToField    (toField)
+
+
+runWrite :: IO ()
+runWrite = listen writePosInfos
     
-printPosInfo :: ConduitT PosInfo Void IO ()
-printPosInfo = do
+runPrint :: IO ()
+runPrint = listen printPosInfos
+
+
+
+type PosInfo = Map String String
+
+data StopInfo = StopInfo {
+        stopCode :: String, name :: String,
+        gpsLat :: Float, gpsLon :: Float
+    } deriving Show
+
+instance SQL.FromRow StopInfo where
+    fromRow = StopInfo <$> SQL.field <*> SQL.field <*> SQL.field <*> SQL.field
+
+
+
+printPosInfos :: ConduitT PosInfo Void IO ()
+printPosInfos = do
     posinfo <- await
     case posinfo of
         Nothing -> return ()
         Just a -> do
             liftIO $ print a
-            printPosInfo
+            printPosInfos
 
-writeToDatabase :: ConduitT PosInfo Void IO ()
-writeToDatabase = do
+writePosInfos :: ConduitT PosInfo Void IO ()
+writePosInfos = do
     dbconn <- liftIO $ SQL.open "database2.db"
     liftIO $ SQL.execute_ dbconn
         "CREATE TABLE IF NOT EXISTS actual_arrivals ( \
@@ -69,22 +106,74 @@ writeToDatabase = do
                 [
                     toField (a ! "timestamp"),
                     toField (a ! "userstopcode"),
-                    toField ((read (a ! "punctuality")) :: Int),
-                    toField ((read (a ! "journeynumber")) :: Int),
+                    toField (read (a ! "punctuality") :: Int),
+                    toField (read (a ! "journeynumber") :: Int),
                     toField (a ! "lineplanningnumber"),
                     toField (a ! "type")
                 ]
 
-            writeToDatabase
+            writePosInfos
 
 
-type PosInfo = Map String String
+
+
+listen :: ConduitT PosInfo Void IO () -> IO ()
+listen downstream = listenKV6 (filterPosInfo downstream)
+
+listenKV6 :: ConduitT PosInfo Void IO () -> IO ()
+listenKV6 downstream =
+    WS.runClient "localhost" 9160 "/" (
+        \conn -> C.runConduit $ listenKV6' conn .| downstream
+    )
+
+
+getStop :: SQL.Connection -> String -> IO (Maybe StopInfo)
+getStop dbconn stopcode = do
+    stops <- SQL.query dbconn "SELECT * FROM stops WHERE stop_code = ? LIMIT 1"
+                            [stopcode] :: IO [StopInfo]
+    pure $ case stops of
+        [] -> Nothing
+        x:_ -> Just x
+    
+
+includeUpdate :: SQL.Connection -> PosInfo -> IO Bool
+includeUpdate dbconn m =
+    let t = case Map.lookup "type" m of
+            Just a -> a
+            Nothing -> error $ "Missing type in " ++ show m
+        isGoodType = t == "DEPARTURE" || t == "ARRIVAL"
+    in
+        case isGoodType of
+            False -> pure False
+            True -> do
+                stop <- getStop dbconn (
+                    case Map.lookup "userstopcode" m of
+                        Just a -> a
+                        Nothing -> error $ "Missing userstopcode in " ++ show m)
+                pure $ isJust stop
+
+
+
+filterConduit :: (a -> IO Bool) -> ConduitT a a IO ()
+filterConduit f = do
+    a <- C.await
+    case a of
+        Nothing -> return ()
+        Just a' -> do
+            include <- liftIO $ f a'
+            if include then do
+                C.yield a'
+                filterConduit f
+            else
+                filterConduit f
+
+-- Internals
 
 limitText :: BS.ByteString -> Data.Text.Text
 limitText s =
     let a = T.take 31 . TL.toStrict . TLE.decodeUtf8 $ s
     in (
-        if (T.length a) > 30
+        if T.length a > 30
         then a <> "..."
         else a
        )
@@ -116,61 +205,16 @@ extractPosInfo rawS =
     in
         Prelude.map parseXMLposinfo posinfo
 
-getStop :: SQL.Connection -> String -> IO (Maybe StopInfo)
-getStop dbconn stopcode = do
-    stops <- SQL.query dbconn "SELECT * FROM stops WHERE stop_code = ? LIMIT 1"
-                            [stopcode] :: IO [StopInfo]
-    pure $ case stops of
-        [] -> Nothing
-        x:_ -> Just x
-    
-
-includeUpdate :: SQL.Connection -> PosInfo -> IO Bool
-includeUpdate dbconn m =
-    let t = case (Map.lookup "type" m) of
-            Just a -> a
-            Nothing -> error $ "Missing type in " ++ show m
-        isGoodType = t == "DEPARTURE" || t == "ARRIVAL"
-    in
-        case isGoodType of
-            False -> pure False
-            True -> do
-                stop <- getStop dbconn (
-                    case (Map.lookup "userstopcode" m) of
-                        Just a -> a
-                        Nothing -> error $ "Missing userstopcode in " ++ show m)
-                pure $ isJust stop
-
-data StopInfo = StopInfo {
-        stopCode :: String, name :: String,
-        gpsLat :: Float, gpsLon :: Float
-    } deriving Show
-
-instance SQL.FromRow StopInfo where
-    fromRow = StopInfo <$> SQL.field <*> SQL.field <*> SQL.field <*> SQL.field
 
 decompressMessage :: WS.DataMessage -> (String, BS.ByteString)
 decompressMessage (WS.Text a _)
     = ("text data (direct)", a)
-decompressMessage (WS.Binary a) | (BS.head a) == 0x1f
+decompressMessage (WS.Binary a) | BS.head a == 0x1f
     = ("compressed data", GZip.decompress a)
 decompressMessage (WS.Binary a)
     = ("data", a)
 
-filterConduit :: (a -> IO Bool) -> ConduitT a a IO ()
-filterConduit f = do
-    a <- C.await
-    case a of
-        Nothing -> return ()
-        Just a' -> do
-            include <- liftIO $ f a'
-            if include then do
-                C.yield a'
-                filterConduit f
-            else
-                filterConduit f
-
-filterPosInfo :: (ConduitT PosInfo Void IO ()) -> ConduitT PosInfo Void IO ()
+filterPosInfo :: ConduitT PosInfo Void IO () -> ConduitT PosInfo Void IO ()
 filterPosInfo dst = do
     dbconn <- liftIO $ SQL.open "database2.db"
     filterConduit (includeUpdate dbconn) .| dst
@@ -179,14 +223,6 @@ filterPosInfo dst = do
     liftIO $ SQL.close dbconn
 
 
-listen :: ConduitT PosInfo Void IO () -> IO ()
-listen downstream = listenKV6 (filterPosInfo downstream)
-
-listenKV6 :: ConduitT PosInfo Void IO r -> IO r
-listenKV6 downstream =
-    WS.runClient "localhost" 9160 "/" (
-        \conn -> C.runConduit $ (listenKV6' conn) .| downstream
-    )
 
 listenKV6' :: WS.Connection -> ConduitT () PosInfo IO ()
 listenKV6' conn = do
@@ -200,6 +236,11 @@ listenKV6' conn = do
             _ -> return ()
     pure ()
 
+
+-- Old
+
+runPrintOld :: IO ()
+runPrintOld = WS.runClient "localhost" 9160 "/" testing
 
 testing :: WS.ClientApp ()
 testing conn = do
@@ -216,12 +257,12 @@ testing conn = do
 
         let (msgType, msgData) = decompressMessage msg
 
-        let updates = (if (msgType == "compressed data") then
-                (extractPosInfo msgData) else [])
+        let updates = (if msgType == "compressed data" then
+                extractPosInfo msgData else [])
 
         updates' <- liftIO $ filterM (includeUpdate dbconn) updates
 
-        liftIO $ putStrLn $ show updates'
+        liftIO $ print updates'
 
         pure ()
 
